b2121d7761f1861afd548a26800c81e6417f012b
' <'>
2014-03-17 14:51:22 -0500

    done with prt 5

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content Sample App" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+describe "Help page" do
+    it "should have the content Help Page" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/help'
+      page.should have_content('Help Page')
+    end
+  end
+end
