f5c3e106f3e5da31a2b9a7594fda4b370920b6a3
' <'>
2014-03-19 14:50:19 -0500

    yay more completion

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,11 +8,18 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
   end
-describe "Help page" do
+  describe "Help page" do
     it "should have the content Help Page" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
      visit '/static_pages/help'
       page.should have_content('Help Page')
+    end
+  end
+  describe "About Us page" do
+    it "should have the content About Us" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/about'
+      page.should have_content('About Us')
     end
   end
 end
